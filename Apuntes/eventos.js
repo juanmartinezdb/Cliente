c07d2be081e41ce909b7bec4c93efd7f6f0c2a65
juanmartinezdb <juanmmcontacto@gmail.com>
2024-10-25 10:04:03 +0200

    formulario

---
+++ Apuntes/eventos.js
@@ -0,0 +1,8 @@
+// En un evento, cuando preguntamos a event, por el type,
+// se refiere al tipo de evento, es decir si es un keyoardEvent
+// o es un click, o lo que sea
+
+/*
+El change sucede cuando se sale del contenido que se esta tocand
+y si se ha producido un cambio
+*/  
