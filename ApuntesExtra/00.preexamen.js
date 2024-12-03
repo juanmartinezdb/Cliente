c253bf3c89fbc53e2378cd0335cd9788e95d8a3a
juanmartinezdb <juanmmcontacto@gmail.com>
2024-12-03 14:44:48 +0100

    notas para examen

---
+++ ApuntesExtra/00.preexamen.js
@@ -0,0 +1,10 @@
+
+const objeto = { a: 1, b: 2, c: 3 };
+for (key in objeto) {
+    console.log(key);
+}
+
+const claves =Object.keys(objeto) //nos devuelve un array con las "claves" del objeto.
+const valores =Object.values(objeto) // nos devuelve un array con "valor" del objeto.
+
+va a ser de encontrar en un json , mas que de mostrar
