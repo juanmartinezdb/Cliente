96fc01dc1c1c5931b9a51454d9abcc5b0c32d33e
juanmartinezdb <juanmmcontacto@gmail.com>
2025-01-27 09:35:25 +0100

    cosas clase

--- ANGULAR/movilesA20/src/app/app.component.ts
+++ ANGULAR/movilesA20/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {  HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, HttpClient, BrowserModule,
-    ReactiveFormsModule,],
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule, BrowserModule,
+    ReactiveFormsModule, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ANGULAR/movilesA20/src/app/app.routes.ts
+++ ANGULAR/movilesA20/src/app/app.routes.ts
@@ -1,2 +1,14 @@
 import { Routes } from '@angular/router';
-export const routes: Routes = [];
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
+export const routeConfig: Routes = [
+    {path: '', component: ProductListComponent },
+    {path: 'products/:productId', component: ProductDetailsComponent},
+    {path:'cart', component: CartComponent},
+    {path:'shipping', component: ShippingComponent},
+];
+
+export default routeConfig;
